fd471e6ae96b328bf308a1567b9f7adc342adb73
Rodrigo Lousada <rodrigo.m.lousada@gmail.com>
2017-04-05 11:43:31 +0100

    P2 continue

---
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
@@ -0,0 +1,35 @@
+package org.komparator.mediator.ws;
+
+import javax.jws.WebService;
+
+// TODO annotate to bind with WSDL
+// TODO implement port type interface
+public class MediatorPortImpl {
+
+	// end point manager
+	private MediatorEndpointManager endpointManager;
+
+	public MediatorPortImpl(MediatorEndpointManager endpointManager) {
+		this.endpointManager = endpointManager;
+	}
+
+	// Main operations -------------------------------------------------------
+
+    // TODO
+	
+    
+	// Auxiliary operations --------------------------------------------------	
+	
+    // TODO
+
+	
+	// View helpers -----------------------------------------------------
+	
+    // TODO
+
+    
+	// Exception helpers -----------------------------------------------------
+
+    // TODO
+
+}
