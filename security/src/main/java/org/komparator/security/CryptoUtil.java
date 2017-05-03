e928035aa19eea4f664c745023c1657af94755e4
Rodrigo Lousada <rodrigo.m.lousada@gmail.com>
2017-05-03 15:00:39 +0100

    P3_continue

---
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -0,0 +1,12 @@
+package org.komparator.security;
+
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+import java.util.*;
+
+public class CryptoUtil {
+
+    // TODO add security helper methods
+
+}
