10cc6a59cea7a5c22caf5f84212f5874e51032d8
Rodrigo Lousada <rodrigo.m.lousada@gmail.com>
2017-05-03 18:39:28 +0100

    TemporalValidationHandler

---
+++ security/src/main/java/org/komparator/security/handler/TemporalValidationHandler.java
@@ -0,0 +1,135 @@
+package org.komparator.security.handler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.MessageContext.Scope;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+public class TemporalValidationHandler implements SOAPHandler<SOAPMessageContext> {
+	
+	public static final String CONTEXT_PROPERTY = "my.property";
+
+	/** Date formatter used for outputting timestamps in ISO 8601 format */
+	private SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	
+	@Override
+	public boolean handleMessage(SOAPMessageContext smc) {
+		System.out.println("AddTemporalValidationHandler: Handling message.");
+
+		Boolean outboundElement = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+		try {
+			if (outboundElement.booleanValue()) {
+				System.out.println("Writing header in outbound SOAP message...");
+
+				// get SOAP envelope
+				SOAPMessage msg = smc.getMessage();
+				SOAPPart sp = msg.getSOAPPart();
+				SOAPEnvelope se = sp.getEnvelope();
+
+				// add header
+				SOAPHeader sh = se.getHeader();
+				if (sh == null)
+					sh = se.addHeader();
+
+				// add header element (name, namespace prefix, namespace)
+				Name name = se.createName("tempValidationHeader", "t", "http://org.komparator.A63");
+				SOAPHeaderElement element = sh.addHeaderElement(name);
+
+				// add header element value
+				String valueString = dateFormatter.format(new Date());
+				element.addTextNode(valueString);
+
+				
+			} else {
+				System.out.println("Reading header in inbound SOAP message...");
+
+				// get SOAP envelope header
+				SOAPMessage msg = smc.getMessage();
+				SOAPPart sp = msg.getSOAPPart();
+				SOAPEnvelope se = sp.getEnvelope();
+				SOAPHeader sh = se.getHeader();
+
+				// check header
+				if (sh == null) {
+					System.out.println("Header not found.");
+					return true;
+				}
+
+				// get first header element
+				Name name = se.createName("tempValidationHeader", "t", "http://org.komparator.A63");
+				Iterator it = sh.getChildElements(name);
+				// check header element
+				if (!it.hasNext()) {
+					System.out.println("Header element not found.");
+					return true;
+				}
+				SOAPElement element = (SOAPElement) it.next();
+
+				// get header element value
+				String valueString = element.getValue();
+				Date sending_date = dateFormatter.parse(valueString);
+				//Thread.sleep(4000);
+				Date receiving_date = new Date();
+				
+				if(receiving_date.getTime() - sending_date.getTime() > 3000) {
+					System.out.println("Time Limit exceeded.");
+					return false;
+				}
+				
+				// int value = Integer.parseInt(valueString);
+
+				
+				// print received header
+				System.out.println("Header value is " + valueString);
+				
+				
+				/*
+				// put header in a property context
+				smc.put(CONTEXT_PROPERTY, value);
+				// set property scope to application client/server class can
+				// access it
+				smc.setScope(CONTEXT_PROPERTY, Scope.APPLICATION);
+				*/
+			}
+		} catch (Exception e) {
+			System.out.print("Caught exception in handleMessage: ");
+			System.out.println(e);
+			System.out.println("Continue normal processing...");
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean handleFault(SOAPMessageContext smc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void close(MessageContext smc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<QName> getHeaders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
