1a4c4390e641eee0add256602453f7f52409fde2
JotaMoreira <joao123moreira@gmail.com>
2017-05-04 19:47:29 +0100

    Chipered metods and tests implemented

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -10,7 +10,17 @@ import javax.crypto.NoSuchPaddingException;
 
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 
 import static javax.xml.bind.DatatypeConverter.printHexBinary;
 import static javax.xml.bind.DatatypeConverter.printBase64Binary;
@@ -22,72 +32,118 @@ public class CryptoUtil {
 	
 
 	
-    private byte[] asymcipher(byte[] bytes, Key key){
-    	
-		try {
-			
+    public byte[] asymcipher(byte[] bytes, PublicKey Publickey) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException{
 			Cipher cipher = Cipher.getInstance(ASYM_CIPHER);
-			
-			try {
-				cipher.init(Cipher.ENCRYPT_MODE, key);
-			} catch (InvalidKeyException e) {
-				e.printStackTrace();
-			}
-			
-			try {
-				byte [] cipheredBytes = cipher.doFinal(bytes);
-				return cipheredBytes;
-			} catch (IllegalBlockSizeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (BadPaddingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+			cipher.init(Cipher.ENCRYPT_MODE, Publickey);
+			byte [] cipheredBytes = cipher.doFinal(bytes);
+			return cipheredBytes;
     }
     
-    private byte[] asymdecipher(byte[] bytes, Key key){
-    	
+    public byte[] asymdecipher(byte[] bytes, PrivateKey Privatekey) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
     	Cipher cipher;
-		try {
-			cipher = Cipher.getInstance(ASYM_CIPHER);
-			
-			try {
-				cipher.init(Cipher.DECRYPT_MODE, key);
-			} catch (InvalidKeyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	byte[] decipheredBytes;
-			try {
-				decipheredBytes = cipher.doFinal(bytes);
-				return decipheredBytes;
-			} catch (IllegalBlockSizeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (BadPaddingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	   
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
+		cipher = Cipher.getInstance(ASYM_CIPHER);
+		cipher.init(Cipher.DECRYPT_MODE, Privatekey);
+    	byte[] decipheredBytes;
+		decipheredBytes = cipher.doFinal(bytes);
+		return decipheredBytes;
     }
+    
+    public static PrivateKey getPrivateKeyFromKeyStore(String keyAlias, char[] keyPassword, KeyStore keystore)
+			throws KeyStoreException, UnrecoverableKeyException {
+		PrivateKey key;
+		try {
+			key = (PrivateKey) keystore.getKey(keyAlias, keyPassword);
+		} catch (NoSuchAlgorithmException e) {
+			throw new KeyStoreException(e);
+		}
+		return key;
+	}
+    
+    public static PrivateKey getPrivateKeyFromKeyStoreResource(String keyStoreResourcePath, char[] keyStorePassword,
+			String keyAlias, char[] keyPassword)
+			throws FileNotFoundException, KeyStoreException, UnrecoverableKeyException {
+		KeyStore keystore = readKeystoreFromResource(keyStoreResourcePath, keyStorePassword);
+		return getPrivateKeyFromKeyStore(keyAlias, keyPassword, keystore);
+	}
+    
+    public static PrivateKey getPrivateKeyFromKeyStoreFile(File keyStoreFile, char[] keyStorePassword, String keyAlias,
+			char[] keyPassword) throws FileNotFoundException, KeyStoreException, UnrecoverableKeyException {
+		KeyStore keystore = readKeystoreFromFile(keyStoreFile, keyStorePassword);
+		return getPrivateKeyFromKeyStore(keyAlias, keyPassword, keystore);
+	}
+    
+    public static PrivateKey getPrivateKeyFromKeyStoreFile(String keyStoreFilePath, char[] keyStorePassword,
+			String keyAlias, char[] keyPassword)
+			throws FileNotFoundException, KeyStoreException, UnrecoverableKeyException {
+		return getPrivateKeyFromKeyStoreFile(new File(keyStoreFilePath), keyStorePassword, keyAlias, keyPassword);
+	}
+    
+    private static KeyStore readKeystoreFromStream(InputStream keyStoreInputStream, char[] keyStorePassword)
+			throws KeyStoreException {
+		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+		try {
+			keystore.load(keyStoreInputStream, keyStorePassword);
+		} catch (NoSuchAlgorithmException | CertificateException | IOException e) {
+			throw new KeyStoreException("Could not load key store", e);
+		} finally {
+			closeStream(keyStoreInputStream);
+		}
+		return keystore;
+	}
+    
+    public static KeyStore readKeystoreFromResource(String keyStoreResourcePath, char[] keyStorePassword)
+			throws KeyStoreException {
+		InputStream is = getResourceAsStream(keyStoreResourcePath);
+		return readKeystoreFromStream(is, keyStorePassword);
+	}
+    
+    private static KeyStore readKeystoreFromFile(File keyStoreFile, char[] keyStorePassword)
+			throws FileNotFoundException, KeyStoreException {
+		FileInputStream fis = new FileInputStream(keyStoreFile);
+		return readKeystoreFromStream(fis, keyStorePassword);
+	}
+    
+    public static KeyStore readKeystoreFromFile(String keyStoreFilePath, char[] keyStorePassword)
+			throws FileNotFoundException, KeyStoreException {
+		return readKeystoreFromFile(new File(keyStoreFilePath), keyStorePassword);
+	}
+    
+
+ 	private static InputStream getResourceAsStream(String resourcePath) {
+ 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
+ 		return is;
+ 	}
+
+
+ 	private static void closeStream(InputStream in) {
+ 		try {
+ 			if (in != null)
+ 				in.close();
+ 		} catch (IOException e) {
+ 			// ignore
+ 		}
+ 	}
+ 	
+ 	public static Certificate getX509CertificateFromResource(String certificateResourcePath)
+			throws IOException, CertificateException {
+		InputStream is = getResourceAsStream(certificateResourcePath);
+		return getX509CertificateFromStream(is);
+	}
+ 	
+ 	public static PublicKey getPublicKeyFromCertificate(Certificate certificate) {
+		return certificate.getPublicKey();
+	}
+ 	
+ 	public static Certificate getX509CertificateFromStream(InputStream in) throws CertificateException {
+		try {
+			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+			Certificate cert = certFactory.generateCertificate(in);
+			return cert;
+		} finally {
+			closeStream(in);
+		}
+	}
+ 	  
+ 	
 
 }
--- security/src/test/java/org/komparator/security/CryptoUtilTest.java
+++ security/src/test/java/org/komparator/security/CryptoUtilTest.java
@@ -2,6 +2,8 @@ package org.komparator.security;
 
 import java.io.*;
 import java.security.*;
+import java.security.cert.CertificateException;
+
 import javax.crypto.*;
 import java.util.*;
 
@@ -9,8 +11,19 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class CryptoUtilTest {
+	
+	private final String plainText = "This is the plain text!";
+	/** Plain text bytes. */
+	private final byte[] plainBytes = plainText.getBytes();
+	
+	private static CryptoUtil cryptoutil;
+	
+	private String store_password = "1nsecure";
+	private String key_password = "ins3cur3";
+	
+	private static final int ASYM_KEY_SIZE = 2048;
+	private static final String ASYM_ALGO = "RSA";
 
-    // static members
 
     // one-time initialization and clean-up
     @BeforeClass
@@ -28,7 +41,7 @@ public class CryptoUtilTest {
     // initialization and clean-up for each test
     @Before
     public void setUp() {
-        // runs before each test
+    	cryptoutil= new CryptoUtil();
     }
 
     @After
@@ -38,11 +51,58 @@ public class CryptoUtilTest {
 
     // tests
     @Test
-    public void test() {
-        // do something ...
+    public void success() throws CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+    	java.security.cert.Certificate certificate = CertUtil.getX509CertificateFromResource("example.cer");
+    	PublicKey publickey = CertUtil.getPublicKeyFromCertificate(certificate);
+    	
+		byte[] cipheredBytes = cryptoutil.asymcipher(plainBytes, publickey);
+		assertNotNull(cipheredBytes);
+		
+		PrivateKey privatekey = CertUtil.getPrivateKeyFromKeyStoreResource("example.jks", store_password.toCharArray(), "example", key_password.toCharArray());
+		
+    	byte[] decipheredBytes = cryptoutil.asymdecipher(cipheredBytes, privatekey);
+    	assertNotNull(decipheredBytes);
+    	
+    	String decipheredText = new String(decipheredBytes);
+    	
+    	assertEquals(decipheredText, plainText);
+    	
+    	//buscar certificado, buscar public key do certificado(.cer), encriptar cm public, sacar private do example, decriptar cm private, passar decriptados para string e comparar
+    }
+    
+    @Test (expected=BadPaddingException.class)
+    public void falseKeyTest() throws CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+    	
+    	java.security.cert.Certificate certificate = CertUtil.getX509CertificateFromResource("example.cer");
+    	PublicKey publickey = CertUtil.getPublicKeyFromCertificate(certificate);
+    	
+		byte[] cipheredBytes = cryptoutil.asymcipher(plainBytes, publickey);
+		assertNotNull(cipheredBytes);
+    	
+    	KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ASYM_ALGO);
+    	keyGen.initialize(ASYM_KEY_SIZE);
+    	KeyPair keypair = keyGen.generateKeyPair();
+    	
+    	byte[] decipheredBytes = cryptoutil.asymdecipher(cipheredBytes, keypair.getPrivate());
+   
+    	//publica igual, privado é falsa, mesmo processo com expcted error
 
-        // assertEquals(expected, actual);
-        // if the assert fails, the test fails
+    }
+    
+    @Test(expected=BadPaddingException.class)
+    public void TemperedByte() throws CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+    	java.security.cert.Certificate certificate = CertUtil.getX509CertificateFromResource("example.cer");
+    	PublicKey publickey = CertUtil.getPublicKeyFromCertificate(certificate);
+    	
+		byte[] cipheredBytes = cryptoutil.asymcipher(plainBytes, publickey);
+		assertNotNull(cipheredBytes);
+		cipheredBytes[0]='j';
+		
+		PrivateKey privatekey = CertUtil.getPrivateKeyFromKeyStoreResource("example.jks", store_password.toCharArray(), "example", key_password.toCharArray());
+		
+    	byte[] decipheredBytes = cryptoutil.asymdecipher(cipheredBytes, privatekey);
+    	
+    	//igual ao success mas mudar um byte, text[0]='j'
     }
 
 }
